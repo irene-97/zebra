857b63f59099550da125e9361a31bc65e382a1fa
Irene Artemova <artemova.irene@gmail.com>
2017-10-26 11:53:27 +0400

    Сверстала баннер со сдайдером

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -2,3 +2,4 @@
 // @include('../../node_modules/bowser/bowser.js')
 // @include('../../node_modules/svg4everybody/dist/svg4everybody.js')
 // @include('../../node_modules/jquery/dist/jquery.js')
+// @include('../../node_modules/slick-carousel/slick/slick.js')
